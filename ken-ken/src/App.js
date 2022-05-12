99c6fb4ff0972b6767a6b825385fe644ab465bcb
abdumamdouh <eabdogfx@gmail.com>
2022-05-12 13:31:11 +0200

    implement grid first look

--- ken-ken/src/App.js
+++ ken-ken/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import TextField from "@mui/material/TextField";
 
-function App() {
+const Input = () => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "85px",
+      height: "85px",
+      border: "solid 2px black"
+      // borderTop: "solid 2px black",
+      // borderBottom: "solid 2px black",
+      // borderRight: "solid 2px black",
+      // borderLeft: "solid 2px black"
+    }}
+  >
+    <span style={{ marginLeft: "5px", alignSelf: "start" }}>244</span>
+    <TextField
+      style={{ width: 50, marginBottom: "15px" }}
+      id="outlined-basic"
+      variant="outlined"
+    />
+  </div>
+);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(5, 1fr)",
+        gridTemplateRows: "repeat(5, 1fr)",
+        gridGap: "0 rem"
+      }}
+    >
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </div>
   );
 }
 
-export default App;
