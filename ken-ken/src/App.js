aa28a0b651411c2e904141b88c40a0e0de2e4f08
abdumamdouh <eabdogfx@gmail.com>
2022-05-15 21:05:35 +0200

    minor edit

--- ken-ken/src/App.js
+++ ken-ken/src/App.js
@@ -5,58 +5,60 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "./Modal";
 
-const App = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      width: "100%",
-      margin: "auto",
-    }}
-  >
-    <Typography
-      variant="h2"
-      gutterBottom
-      component="div"
-      style={{ marginTop: "70px", marginBottom: "1px" }}
-    >
-      KenKen
-    </Typography>
-    <Stack spacing={5} direction="row" style={{ margin: "30px 0" }}>
-      <Modal />
-      <Button variant="contained" color="success">
-        Submit
-      </Button>
-    </Stack>
+const size = 5;
+
+const App = () => {
+  const generateCells = () => {
+    const cells = [];
+    for (let i = 0; i < size * size; i++) cells.push(<Cell key={i} />);
+    return cells;
+  };
+  return (
     <div
       style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(5, 85px)",
-        gridTemplateRows: "repeat(5, 85px)",
-        gap: "0",
-        rowGap: "0",
-        columnGap: "0",
-        width: "70%",
-        height: "70%",
+        display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        // marginTop: 'auto',
-        // marginRight: 'auto',
-        // marginLeft: 'auto',
+        flexDirection: "column",
+        width: "100%",
+        margin: "auto",
       }}
     >
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
+      <Typography
+        variant="h2"
+        gutterBottom
+        component="div"
+        style={{ marginTop: "70px", marginBottom: "1px" }}
+      >
+        KenKen
+      </Typography>
+      <Stack spacing={5} direction="row" style={{ margin: "30px 0" }}>
+        <Modal />
+        <Button variant="contained" color="success">
+          Submit
+        </Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${size}, 85px)`,
+          gridTemplateRows: `repeat(${size}, 85px)`,
+          gap: "0",
+          rowGap: "0",
+          columnGap: "0",
+          width: "70%",
+          height: "70%",
+          justifyContent: "center",
+          alignItems: "center",
+          // marginTop: 'auto',
+          // marginRight: 'auto',
+          // marginLeft: 'auto',
+        }}
+      >
+        {generateCells()}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
