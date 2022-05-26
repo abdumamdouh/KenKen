1641e8955ee5c0ce0e45140573c1741c9f8e1782
abdumamdouh <eabdogfx@gmail.com>
2022-05-26 20:21:19 +0200

    make cells dunamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Cell from "./Cell";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
@@ -7,7 +8,77 @@ import Modal from "./Modal";
 
 const size = 5;
 
+const request = {
+  result: [3, 2, 1, 1, 3, 2, 2, 1, 3],
+  values: [
+    {
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      value: "*3",
+    },
+    {
+      bottom: 0,
+      left: 0,
+      right: 1,
+      top: 0,
+      value: "0",
+    },
+    {
+      bottom: 1,
+      left: 1,
+      right: 0,
+      top: 0,
+      value: "+4",
+    },
+    {
+      bottom: 1,
+      left: 0,
+      right: 1,
+      top: 0,
+      value: "*12",
+    },
+    {
+      bottom: 1,
+      left: 1,
+      right: 0,
+      top: 0,
+      value: "0",
+    },
+    {
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: 1,
+      value: "0",
+    },
+    {
+      bottom: 0,
+      left: 0,
+      right: 1,
+      top: 1,
+      value: "0",
+    },
+    {
+      bottom: 0,
+      left: 1,
+      right: 0,
+      top: 1,
+      value: "0",
+    },
+    {
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      value: "*3",
+    },
+  ],
+};
+
 const App = () => {
+  const [cells, setCells] = useState(request);
   const generateCells = () => {
     const cells = [];
     for (let i = 0; i < size * size; i++) cells.push(<Cell key={i} />);
@@ -22,6 +93,7 @@ const App = () => {
         flexDirection: "column",
         width: "100%",
         margin: "auto",
+        gap: "0px",
       }}
     >
       <Typography
@@ -41,8 +113,10 @@ const App = () => {
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: `repeat(${size}, 85px)`,
-          gridTemplateRows: `repeat(${size}, 85px)`,
+          gridTemplateColumns: `repeat(${Math.sqrt(
+            cells.values.length
+          )}, 85px)`,
+          gridTemplateRows: `repeat(${Math.sqrt(cells.values.length)}, 85px)`,
           gap: "0",
           rowGap: "0",
           columnGap: "0",
@@ -50,13 +124,13 @@ const App = () => {
           height: "70%",
           justifyContent: "center",
           alignItems: "center",
-          // marginTop: 'auto',
-          // marginRight: 'auto',
-          // marginLeft: 'auto',
           marginBottom: "200px",
         }}
       >
-        {generateCells()}
+        {/* {generateCells()} */}
+        {cells.values.map((cell, i) => (
+          <Cell key={i} cell={cell} />
+        ))}
       </div>
     </div>
   );
--- src/Cell.js
+++ src/Cell.js
@@ -1,31 +1,35 @@
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 
-const Cell = () => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "85px",
-      height: "85px",
-      outline: "solid 2px black",
-      // borderTop: "solid 2px black",
-      // borderBottom: "solid 2px black",
-      // borderRight: "solid 2px black",
-      // borderLeft: "solid 2px black"
-    }}
-  >
-    <span style={{ marginLeft: "5px", alignSelf: "start" }}>244</span>
-    <TextField
-      style={{ width: 50, marginBottom: "15px" }}
-      id="outlined-basic"
-      variant="outlined"
-      // value={5}
-      // disabled
-    />
-  </div>
-);
+const Cell = ({ cell }) => {
+  const { bottom, left, right, top, value } = cell;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "85px",
+        height: "85px",
+        // outline: "solid 2px black",
+        borderTop: top ? "solid 2px black" : "solid 1px gray",
+        borderBottom: bottom ? "solid 2px black" : "solid 1px gray",
+        borderRight: right ? "solid 2px black" : "solid 1px gray",
+        borderLeft: left ? "solid 2px black" : "solid 1px gray",
+        border: "solid 1px gray",
+      }}
+    >
+      <span style={{ marginLeft: "5px", alignSelf: "start" }}>{value}</span>
+      <TextField
+        style={{ width: 50, marginBottom: "15px" }}
+        id="outlined-basic"
+        variant="outlined"
+        // value={5}
+        // disabled
+      />
+    </div>
+  );
+};
 
 export default Cell;
